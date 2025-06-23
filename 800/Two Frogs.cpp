3ac099df744ec599164c19a1d60af4ea8b274ed5
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2025-06-23 18:58:01 +0600

    added the code " Two Frogs "

---
+++ 800/Two Frogs.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+using ll = long long ;
+
+
+
+
+void solve(){
+    int n ; cin >>n ;
+
+
+    cout << 1 << " " ;
+
+
+    for(int i =n ;i>=3 ;i--){
+        cout << i << " " ;
+    }
+
+    cout << 2 << endl ;
+
+
+
+
+
+}
+
+
+
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL) ;
+
+    int t ; cin >> t ;
+    
+
+
+    while(t--){
+        solve() ;
+    }
+
+
+
+
+}
+
+
+
